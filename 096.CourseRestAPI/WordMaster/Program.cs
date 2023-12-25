10e88c8cca0f6c19a067462f7de32e25ffbe58f9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2023-12-25 01:32:00 +0100

    logger working

--- 096.CourseRestAPI/WordMaster/Program.cs
+++ 096.CourseRestAPI/WordMaster/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using WordMaster.Database;
 using WordMaster.Services;
+using NLog;
+using NLog.Web;
 
 namespace WordMaster
 {
@@ -8,40 +10,64 @@ namespace WordMaster
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            //logger
+            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("Init Main");
+            //end logger
 
-            builder.Services.AddControllersWithViews();
-            if (builder.Environment.IsDevelopment())
+            try
             {
-                builder.Services.AddDbContext<MyDbContext>(options => options.UseInMemoryDatabase("MemoryDatabase"));
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Services.AddControllersWithViews();
+
+                if (builder.Environment.IsDevelopment())
+                {
+                    builder.Services.AddDbContext<MyDbContext>(options => options.UseInMemoryDatabase("MemoryDatabase"));
+                }
+                else
+                {
+                    builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WordMasterConnectionString")));
+                }
+                //logger
+
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
+
+                //end logger
+
+                builder.Services.AddScoped<IWordService, WordService>();
+
+                var app = builder.Build();
+
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.Run();
             }
-            else
+            catch (Exception ex)
             {
-                builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WordMasterConnectionString")));
+                logger.Error(ex);
             }
-
-            builder.Services.AddScoped<IWordService, WordService>();
-
-            var app = builder.Build();
-
-            if (!app.Environment.IsDevelopment())
+            finally
             {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+                LogManager.Shutdown();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
         }
     }
 }
