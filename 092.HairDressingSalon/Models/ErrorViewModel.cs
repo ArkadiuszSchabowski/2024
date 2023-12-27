9212603c0904126e580f40c183355079198d2dfc
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2023-12-28 00:56:54 +0100

    HairDressingSalonProject start - implements basic menu, body and images.

---
+++ 092.HairDressingSalon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _092.HairDressingSalon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
