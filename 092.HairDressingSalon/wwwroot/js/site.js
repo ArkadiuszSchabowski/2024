e180b1ce0ce0370641c6fd450b54a24e77c640c3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2023-12-31 19:24:02 +0100

    work with mainPage -refactoring

--- 092.HairDressingSalon/wwwroot/js/site.js
+++ 092.HairDressingSalon/wwwroot/js/site.js
@@ -1,12 +1,43 @@
 ﻿class MainContent {
     constructor() {
-        this.imageSectionOne = document.getElementById("imageSectionOne");
-        this.offerPhotos = document.getElementById("offerPhotos");
+        this.imageSectionIntroduction = document.getElementById("imageSectionIntroduction");
+        this.hairstylesPhotos = document.getElementById("hairstylesPhotos");
+        this.hairStylesArray = [
+            "../images/hairstyles/hair1.jpg",
+            "../images/hairstyles/hair2.jpg",
+            "../images/hairstyles/hair3.jpg"
+        ];
+        this.currentImageIndex = 0;
+        this.cureentOpinion = 0;
+        this.opinionsContent = document.getElementById("opinionsContent");
+        this.btnPreviousOpinion = document.getElementById("btnPreviousOpinion");
+        this.btnNextOpinion = document.getElementById("btnNextOpinion");
+        this.arrayOpinions = [];
     }
+
     Init() {
         this.DisplayMainPhoto();
-        this.AddImagesToDiv();
+        this.DisplayHairstylesPhoto();
+        this.DisplayOpinions();
+        this.SetButtonsAction();
     }
+
+    SetButtonsAction() {
+        this.btnPreviousOpinion.addEventListener("click", () => {
+            if (this.cureentOpinion > 0) {
+                this.cureentOpinion--;
+                this.DisplayOpinions();
+            }
+        });
+
+        this.btnNextOpinion.addEventListener("click", () => {
+            if (this.cureentOpinion < this.arrayOpinions.length - 1) {
+                this.cureentOpinion++;
+                this.DisplayOpinions();
+            }
+        });
+    }
+
     DisplayMainPhoto() {
         let mainPhoto = document.createElement("img");
 
@@ -16,12 +47,43 @@
         mainPhoto.style.height = "100%";
         mainPhoto.style.borderBottom = "1px solid white";
 
-        this.imageSectionOne.appendChild(mainPhoto);
+        this.imageSectionIntroduction.appendChild(mainPhoto);
     }
 
-    AddImagesToDiv() {
+    DisplayHairstylesPhoto() {
+        let firstImage = document.createElement("img");
+        firstImage.style.width = "100%";
+        firstImage.style.height = "100%";
+        firstImage.style.borderRadius = "50%";
+        this.hairstylesPhotos.appendChild(firstImage);
 
+        setInterval(() => {
+            if (this.hairstylesPhotos.firstChild) {
+                this.hairstylesPhotos.removeChild(this.hairstylesPhotos.firstChild);
+            }
+            let newImage = document.createElement("img");
+            newImage.src = this.hairStylesArray[this.currentImageIndex];
+            newImage.style.width = "100%";
+            newImage.style.height = "100%";
+            newImage.style.borderRadius = "50%";
+            this.hairstylesPhotos.appendChild(newImage);
+            if (this.currentImageIndex < this.hairStylesArray.length - 1) {
+                this.currentImageIndex++;
+            } else {
+                this.currentImageIndex = 0;
+            }
+        }, 2000);
+    }
+
+    DisplayOpinions() {
+        this.arrayOpinions = [
+            "Bardzo dobry fryzjer. Będę stałym klientem ;-)",
+            "Polecam! Super fryzura, niskie ceny!",
+            "Pełen profesjonalizm, fryzura spełnia wszystkie moje oczekiwania!"
+        ];
+        this.opinionsContent.innerHTML = this.arrayOpinions[this.cureentOpinion];
     }
 }
+
 let mainContent = new MainContent();
 mainContent.Init();
