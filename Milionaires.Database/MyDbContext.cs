7f92aa7d57e2f24dc777ddf025c30e2a1325ac19
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-01-09 20:16:47 +0100

    add first records to scores table

--- Milionaires.Database/MyDbContext.cs
+++ Milionaires.Database/MyDbContext.cs
@@ -8,7 +8,31 @@ namespace Milionaires.Database
         public DbSet<Score> Scores { get; set; }
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
-                
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Score>().HasData(new Score
+            {
+                Id = 1,
+                Name = "Ania",
+                Result = 500,
+                Date = new DateTime(2023, 1, 10)
+            },
+            new Score
+            {
+                Id = 2,
+                Name = "Maciek",
+                Result = 2000,
+                Date = new DateTime(2023, 1, 10)
+            },
+            new Score
+            {
+                Id = 3,
+                Name = "Lena",
+                Result = 10000,
+                Date = new DateTime(2023, 1, 10)
+            });
         }
     }
 }
