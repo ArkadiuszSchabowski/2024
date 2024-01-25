3a3c4e04c695adc254e4a22483c9e0f73527b42a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-01-25 15:46:18 +0100

    All migrations are done!

--- Milionaires.Database/Migrations/20240125144514_AddPropertyMaxLenghtToEntityScore.Designer.cs
+++ Milionaires.Database/Migrations/20240125144514_AddPropertyMaxLenghtToEntityScore.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Milionaires.Database;
 
@@ -11,9 +12,11 @@ using Milionaires.Database;
 namespace Milionaires.Database.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240125144514_AddPropertyMaxLenghtToEntityScore")]
+    partial class AddPropertyMaxLenghtToEntityScore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Milionaires.Database/Migrations/20240125144514_AddPropertyMaxLenghtToEntityScore.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Milionaires.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyMaxLenghtToEntityScore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Scores",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Scores",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
