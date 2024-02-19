abd44261198c5aadc95949ecd337ce1f334f1313
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-02-20 00:10:42 +0100

    add database and first migration

---
+++ MassageSalon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MassageSalon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MassageSalon/Program.cs
@@ -0,0 +1,37 @@
+using MassageSalon.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace MassageSalon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MassageSalonDatabase")));
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
