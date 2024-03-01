10af2be7746881fa23a7c31a609269de0646f4c9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-03-01 19:27:51 +0100

    relation 1 to 1

---
+++ 085.RelationsDatabase/085.RelationsDatabase.Database/Migrations/20240301182425_SeedData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _085.RelationsDatabase.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "abc@o2.pl", "Arek", "700-600-500" },
+                    { 2, "cdf@o2.pl", "Damian", "100-600-100" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "City", "Country", "Street", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "Kraków", "Polska", "Nowa", 1 },
+                    { 2, "Poznań", "Polska", "Stara", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ 085.RelationsDatabase/085.RelationsDatabase.Database/MyDbContext.cs
@@ -0,0 +1,52 @@
+﻿using _085.RelationsDatabase.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _085.RelationsDatabase.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Adress)
+                .WithOne(a => a.User)
+                .HasForeignKey<Address>(a => a.UserId);
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 1,
+                Name = "Arek",
+                Email = "abc@o2.pl",
+                Phone = "700-600-500"
+            }, new User
+            {
+                Id = 2,
+                Name = "Damian",
+                Email = "cdf@o2.pl",
+                Phone = "100-600-100"
+            });
+            modelBuilder.Entity<Address>().HasData(new Address
+            {
+                Id = 1,
+                Street = "Nowa",
+                City = "Kraków",
+                Country = "Polska",
+                UserId = 1
+            },
+            new Address
+            {
+                Id = 2,
+                Street = "Stara",
+                City = "Poznań",
+                Country = "Polska",
+                UserId = 2
+            });
+        }
+    }
+}
