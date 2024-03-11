910a70607827343975aea29b9cb136396a803cf2
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-03-11 18:00:20 +0100

    add app.js

--- 083.TypeScriptCourse/dist/app.js
+++ 083.TypeScriptCourse/dist/app.js
@@ -1,9 +1,13 @@
 let btn = document.getElementById("btnBuy");
+let price = 50;
+let discountValue = 0.1
 let discount = false;
 
 btn.addEventListener("click", () =>{
     if(discount){
-        console.log("You buy this for price - discount")
+        let newPrice = price * price - (price*discountValue);
+        console.log(`You buy this for ${newPrice}zl`);
+        return;
     }
-    console.log("You buy this for normal price")
-})
+    console.log(`You buy this for ${price} zl`)
+});
