ceb87f86ec7577106001ee8204d2ab41a1c08d7f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-03-11 20:39:48 +0100

    Create SimpleBackend(db and simple REST) and SeedUsers in MSSQL

--- 084.DatingAppAngular/api/MyDbContext.cs
+++ 084.DatingAppAngular/api/MyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using api.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace api
 {
@@ -8,7 +9,26 @@ namespace api
         public DbSet<AppUser> Users { get; set; }
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
-            
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>().HasData(
+                new AppUser
+                {
+                    Id = 1,
+                    UserName = "Świnka"
+                },
+                new AppUser
+                {
+                    Id = 2,
+                    UserName = "Owca"
+                },
+                new AppUser
+                {
+                    Id = 3,
+                    UserName = "Krowa"
+                });
         }
     }
 }
