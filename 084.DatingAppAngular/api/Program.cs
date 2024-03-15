61d5bd1da4a7dffca3c0650d5211d2c75336130c
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-03-15 18:57:05 +0100

    end three course part

--- 084.DatingAppAngular/api/Program.cs
+++ 084.DatingAppAngular/api/Program.cs
@@ -21,7 +21,7 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "DatingApp");
 });
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthorization();
 
 app.MapControllers();
