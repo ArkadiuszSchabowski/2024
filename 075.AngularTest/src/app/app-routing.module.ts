5a544afc88e52d29e939a57dc7eaf290e7fa81b4
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-04-26 22:47:07 +0200

    Add routing guard to our app

--- 075.AngularTest/src/app/app-routing.module.ts
+++ 075.AngularTest/src/app/app-routing.module.ts
@@ -5,18 +5,25 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
-  {path: '**', component: HomePageComponent, pathMatch: 'full'}
+  { path: '', component: HomePageComponent },
+  { path: '', 
+  runGuardsAndResolvers: 'always',
+  canActivate: [authGuard],
+  children: [
+    { path: 'members', component: MemberListComponent},
+    { path: 'members/:id', component: MemberDetailComponent },
+    { path: 'lists', component: ListsComponent },
+    { path: 'messages', component: MessagesComponent },
+  ]
+},
+  { path: '**', component: HomePageComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
