1e12ca445eea1222433d90ec5c183973b89ce724
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-06-05 16:06:27 +0200

    create ApiBaseController

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,14 +1,11 @@
 ﻿using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
