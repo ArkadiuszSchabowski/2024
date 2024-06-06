415bf11afdb809bb8e3247d266aac07c45ad204f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-06-06 20:11:13 +0200

    implement account controller

---
+++ DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string userName, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = userName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+            return Ok(user);
+        }
+    }
+}
